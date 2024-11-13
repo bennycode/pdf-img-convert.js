4f93cecf124fcabac32d618af298b5b200c1793c
Benny Neugebauer <mail@bennycode.com>
2024-11-13 09:25:22 +0100

    fix: Using tagged unions

--- pdf-img-convert.d.ts
+++ pdf-img-convert.d.ts
@@ -1,17 +1,31 @@
+type ConversionConfig = {
+    /** Width of the output image in pixels */
+    width?: number;
+    /** Height of the output image in pixels */
+    height?: number;
+    /** A list of page numbers to render (default is all pages) */
+    page_numbers?: number[];
+    /** Whether to output the image in base64 format */
+    base64?: boolean;
+    /** Scaling ratio for the PDF page viewport */
+    scale?: number;
+};
+
+type PDFData = string | Uint8Array | Buffer;
+
 declare function convert(
-    pdf: string | Uint8Array | Buffer,
-    conversion_config?: {
-            /** Width of the output image in pixels */
-            width?: number;
-            /** Height of the output image in pixels */
-            height?: number;
-            /** A list of page numbers to render (default is all pages) */
-            page_numbers?: number[];
-            /** Whether to output the image in base64 format */
-            base64?: boolean;
-            /** Scaling ratio for the PDF page viewport */
-            scale?: number;
-    }
+    pdf: PDFData, 
+    conversion_config: {base64: true} & ConversionConfig
+): Promise<string[]>;
+
+declare function convert(
+    pdf: PDFData, 
+    conversion_config?: {base64?: false} & ConversionConfig
+): Promise<Uint8Array[]>;
+
+declare function convert(
+    pdf: PDFData,
+    conversion_config?: ConversionConfig
 ): Promise<string[] | Uint8Array[]>;
 
 export {convert};
